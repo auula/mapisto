e3d8802cce16255ab87e8919a7a9c462dc9033e8
Dings <deen.job@qq.com>
2021-06-29 21:37:29 +0800

    updated: license.

--- core/template_go.go
+++ core/template_go.go
@@ -16,21 +16,4 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-package cmd
-
-import "github.com/fatih/color"
-
-var (
-	version = "0.0.1 Alpha"
-	banner  = color.MagentaString("\n" +
-		"    __  ___            _      __      \n" +
-		"   /  |/  /___ _____  (_)____/ /_____ \n" +
-		"  / /|_/ / __ `/ __ \\/ / ___/ __/ __ \\\n" +
-		" / /  / / /_/ / /_/ / (__  ) /_/ /_/ /\n" +
-		"/_/  /_/\\__,_/ .___/_/____/\\__/\\____/ \n" +
-		"            /_/   " + version + "\n")
-	description = banner + color.GreenString(`
-Mapisto is a command-line database tool that can generate tables
-with the type of Golang, Python,Rust, Java, TypeScript language,
-with the command line.`)
-)
+package core
