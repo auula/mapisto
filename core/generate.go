1a588494145f5f968f0fb731fb344283481b8134
Dings <deen.job@qq.com>
2021-06-30 22:31:17 +0800

    add: code template.

--- core/generate.go
+++ core/generate.go
@@ -19,4 +19,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+
 package core
+
+// MEngine is mapisto database mapper engine
+type MEngine struct {
+	tmpl Template
+}
