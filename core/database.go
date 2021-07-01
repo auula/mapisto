7f795b0fd1913f102b67522a1de3c6666f9e7738
Dings <deen.job@qq.com>
2021-07-01 16:03:25 +0800

    add: database data type.

--- core/database.go
+++ core/database.go
@@ -20,6 +20,7 @@ package core
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 )
 
@@ -40,18 +41,18 @@ type TableColumn struct {
 	ColumnComment string
 }
 
-type DBMode struct {
+type DataBase struct {
 	DBEngine *sql.DB
 	DBInfo   *DBInfo
 }
 
-func NewDB(info *DBInfo) *DBMode {
-	return &DBMode{
+func NewDB(info *DBInfo) *DataBase {
+	return &DataBase{
 		DBInfo: info,
 	}
 }
 
-func (db *DBMode) Connection() error {
+func (db *DataBase) Connection() error {
 	var err error
 	str := "%s:%s@tcp(%s)/infomation_schema?charset=%s&parseTime=True&loc=Local"
 	dsn := fmt.Sprintf(str,
@@ -63,4 +64,30 @@ func (db *DBMode) Connection() error {
 	// open sql connection
 	db.DBEngine, err = sql.Open(db.DBInfo.DBType, dsn)
 	return err
+}
+func (db *DataBase) Columns(dbName, tableName string) ([]*TableColumn, error) {
+	query := "SELECT COLUMN_NAME, DATA_TYPE, COLUMN_KEY, " +
+		"IS_NULLABLE, COLUMN_TYPE, COLUMN_COMMENT " +
+		"FROM COLUMNS WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ? "
+	rows, err := db.DBEngine.Query(query, dbName, tableName)
+	if err != nil {
+		return nil, err
+	}
+	if rows == nil {
+		return nil, errors.New("no data")
+	}
+	defer rows.Close()
+
+	var columns []*TableColumn
+	for rows.Next() {
+		var column TableColumn
+		err := rows.Scan(&column.ColumnName, &column.DataType, &column.ColumnKey, &column.IsNullable, &column.ColumnType, &column.ColumnComment)
+		if err != nil {
+			return nil, err
+		}
+
+		columns = append(columns, &column)
+	}
+
+	return columns, nil
 }
